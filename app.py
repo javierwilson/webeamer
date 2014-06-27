c82cde501868d90015387545342ff545b7d8d698
Javier Wilson <javier.wilson@gmail.com>
2014-06-27 17:00:36 +0100

    use local config file, creat slides in html (draft version)

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
-from bottle import Bottle, run, debug, template, route, view, install, request, redirect, post, error
+from bottle import Bottle, run, debug, template, route, view, request, redirect, post, error
 import sqlite3
 
-debug(True)
-#run(reloader=True)
-dbfile = '/tmp/slides.db'
+DEBUG = False
+DBFILE = '/tmp/slides.db'
+
+from local import *
+
+debug(DEBUG)
 
 @route('/setup')
 def setup():
-    conn = sqlite3.connect( dbfile )
+    conn = sqlite3.connect( DBFILE )
     db = conn.cursor()
     db.execute('CREATE TABLE IF NOT EXISTS slides (name, slide, text, PRIMARY KEY(name, slide))')
     conn.close()
 
 @post('/')
-def login():
+def do_login():
     name = request.forms.get('name')
     redirect('/' + name + '/1')
     return
 
 @route('/')
+@view('login.html')
+def login():
+    return
+
 @route('/<name>')
 @route('/<name>/<slide:int>')
 @view('index.html')
 def home(name=None, slide=1):
     if name:
-        conn = sqlite3.connect( dbfile )
+        conn = sqlite3.connect( DBFILE )
         db = conn.cursor()
         row = db.execute('SELECT text FROM slides WHERE name=? AND slide=?', (name, slide) ).fetchone()
-        conn.close()
+        rows = db.execute('SELECT slide,text FROM slides WHERE name=?', (name,) ).fetchall()
         if row:
-            return dict(name=name, slide=slide, text=row[0])
+            return dict(name=name, slide=slide, text=row[0], rows=rows)
         else:
             #return error404(404)
             text = 'Hello Monkey!'
+        conn.close()
     else:
         text = 'Go login or something!'
     
-    return dict(name=name, slide=slide, text=text)
+    return dict(name=name, slide=slide, text=text, rows=rows)
 
 @post('/<name>/<slide:int>')
 @view('index.html')
@@ -57,7 +65,13 @@ def save_slide(name, slide):
     return
 
 
-
+@route('/<name>/go')
+@view('slides.html')
+def generate(name):
+    conn = sqlite3.connect( DBFILE )
+    db = conn.cursor()
+    rows = db.execute('SELECT slide,text FROM slides WHERE name=?', (name,) ).fetchall()
+    return dict(name=name, rows=rows)
 
 @error(404)
 def error404(error):
