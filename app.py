122664b995def61e7a403d9020edb6ae253c925e
Javier Wilson <javier.wilson@gmail.com>
2014-06-28 16:38:45 +0100

    adding title to a presentation

--- app.py
+++ app.py
@@ -12,12 +12,19 @@ debug(DEBUG)
 def setup():
     conn = sqlite3.connect( DBFILE )
     db = conn.cursor()
-    db.execute('CREATE TABLE IF NOT EXISTS slides (name, slide, text, PRIMARY KEY(name, slide))')
+    db.execute('CREATE TABLE IF NOT EXISTS slides (name, slide, title, text, PRIMARY KEY(name, slide))')
+    db.execute('CREATE TABLE IF NOT EXISTS presentations (name PRIMARY KEY, title, text)')
     conn.close()
 
 @post('/')
 def do_login():
-    name = request.forms.get('name')
+    name = request.forms.get('name').decode('utf-8')
+    title = request.forms.get('title').decode('utf-8')
+    conn = sqlite3.connect( DBFILE )
+    db = conn.cursor()
+    db.execute('INSERT INTO presentations (name,title) VALUES (?, ?)', (name, title, ) )
+    conn.commit()
+    conn.close()
     redirect('/' + name + '/1')
     return
 
@@ -33,45 +40,52 @@ def home(name=None, slide=1):
     if name:
         conn = sqlite3.connect( DBFILE )
         db = conn.cursor()
-        row = db.execute('SELECT text FROM slides WHERE name=? AND slide=?', (name, slide) ).fetchone()
-        rows = db.execute('SELECT slide,text FROM slides WHERE name=?', (name,) ).fetchall()
+        presentation = db.execute('SELECT title,text FROM presentations WHERE name=?', (name,) ).fetchone()
+        p_title = presentation[0]
+        p_text = presentation[1]
+        row = db.execute('SELECT title,text FROM slides WHERE name=? AND slide=?', (name, slide) ).fetchone()
+        rows = db.execute('SELECT slide,title,text FROM slides WHERE name=?', (name,) ).fetchall()
         if row:
-            return dict(name=name, slide=slide, text=row[0], rows=rows)
+            return dict(name=name, slide=slide, title=row[0], text=row[1], rows=rows, p_title=p_title)
         else:
             #return error404(404)
-            text = 'Hello Monkey!'
+            title = 'Hello Monkey!'
+            text = 'This is the winter of our discontent.'
         conn.close()
     else:
         text = 'Go login or something!'
     
-    return dict(name=name, slide=slide, text=text, rows=rows)
+    return dict(name=name, slide=slide, text=text, title=title, rows=rows, p_title=p_title)
 
 @post('/<name>/<slide:int>')
 @view('index.html')
 def save_slide(name, slide):
-    text = request.forms.get('text')
-    conn = sqlite3.connect('/tmp/slides.db')
+    title = request.forms.get('title').decode('utf-8')
+    text = request.forms.get('text').decode('utf-8')
+    conn = sqlite3.connect( DBFILE )
     db = conn.cursor()
-    row = db.execute('SELECT text FROM slides WHERE name=? AND slide=?', (name, slide) ).fetchone()
+    row = db.execute('SELECT title,text FROM slides WHERE name=? AND slide=?', (name, slide) ).fetchone()
     if row:
-        db.execute('UPDATE slides SET text=? WHERE name=? AND slide=?', (text, name, slide) )
+        db.execute('UPDATE slides SET title=?, text=? WHERE name=? AND slide=?', (title, text, name, slide) )
     else:
-        db.execute('INSERT INTO slides (name,slide,text) VALUES (?, ?, ?)', (name, slide, text) )
+        db.execute('INSERT INTO slides (name,slide,title,text) VALUES (?, ?, ?, ?)', (name, slide, title, text) )
     conn.commit()
     conn.close()
     next_slide = str( int(slide) + 1 )
     redirect('/' + name + '/' + next_slide)
-    #return dict(name=name, slide=slide, text=text)
     return
 
 
-@route('/<name>/go')
+@route('/<name>/html')
 @view('slides.html')
 def generate(name):
     conn = sqlite3.connect( DBFILE )
     db = conn.cursor()
-    rows = db.execute('SELECT slide,text FROM slides WHERE name=?', (name,) ).fetchall()
-    return dict(name=name, rows=rows)
+    presentation = db.execute('SELECT title,text FROM presentations WHERE name=?', (name,) ).fetchone()
+    p_title = presentation[0]
+    p_text = presentation[1]
+    rows = db.execute('SELECT slide,title,text FROM slides WHERE name=?', (name,) ).fetchall()
+    return dict(name=name, rows=rows, p_title=p_title)
 
 @error(404)
 def error404(error):
